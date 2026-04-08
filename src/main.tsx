b694b220914e3152c5630acc2051d45d86700371
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-08 14:32:28 +0530

    Installed sidebar and react router for app routes.

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,18 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 import "./index.css"
-import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import AppRoutes from "@/features/routing/AppRoutes.tsx"
+import { BrowserRouter } from "react-router-dom"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <BrowserRouter>
+
+      <AppRoutes/>
+      </BrowserRouter>
+      {/*<App />*/}
     </ThemeProvider>
   </StrictMode>
 )
