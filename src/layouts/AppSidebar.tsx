b51fec042cc85cab46db2d8b6367348a965887ec
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-08 15:07:42 +0530

    Created simple sidebar for app.

---
+++ src/layouts/AppSidebar.tsx
@@ -0,0 +1,46 @@
+import React from "react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuBadge,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar.tsx"
+import { HugeiconsIcon } from "@hugeicons/react"
+import {
+  ArrowDataTransferVerticalIcon,
+  Home03Icon,
+} from "@hugeicons/core-free-icons"
+
+const AppSidebar: React.FC = () => {
+  return (
+    <Sidebar>
+      <SidebarHeader>Finance Master</SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Main</SidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Overview</SidebarMenuButton>
+              <SidebarMenuBadge>
+                <HugeiconsIcon icon={Home03Icon} size={20} />
+              </SidebarMenuBadge>
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Transactions</SidebarMenuButton>
+              <SidebarMenuBadge>
+                <HugeiconsIcon icon={ArrowDataTransferVerticalIcon} size={20} />
+              </SidebarMenuBadge>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
+
+export default AppSidebar
--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,20 @@ import "./index.css"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import AppRoutes from "@/features/routing/AppRoutes.tsx"
 import { BrowserRouter } from "react-router-dom"
+import { SidebarProvider } from "@/components/ui/sidebar.tsx"
+import AppSidebar from "@/layouts/AppSidebar.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-
-      <AppRoutes/>
-      </BrowserRouter>
-      {/*<App />*/}
+      <SidebarProvider>
+        <BrowserRouter>
+          <AppSidebar />
+          <main>
+            <AppRoutes />
+          </main>
+        </BrowserRouter>
+      </SidebarProvider>
     </ThemeProvider>
   </StrictMode>
 )
