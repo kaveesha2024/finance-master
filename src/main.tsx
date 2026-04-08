aaa75413ff237233c19da9cc354f82de4c14adde
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-08 15:44:06 +0530

    Installed redux persist for store persistence.

--- src/main.tsx
+++ src/main.tsx
@@ -8,21 +8,24 @@ import { BrowserRouter } from "react-router-dom"
 import { SidebarProvider } from "@/components/ui/sidebar.tsx"
 import AppSidebar from "@/layouts/AppSidebar.tsx"
 import { Provider } from "react-redux"
-import store from "@/store.ts"
+import { persistor, store } from "@/store.ts"
+import { PersistGate } from "redux-persist/integration/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <ThemeProvider>
-        <SidebarProvider>
-          <BrowserRouter>
-            <AppSidebar />
-            <main>
-              <AppRoutes />
-            </main>
-          </BrowserRouter>
-        </SidebarProvider>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider>
+          <SidebarProvider>
+            <BrowserRouter>
+              <AppSidebar />
+              <main>
+                <AppRoutes />
+              </main>
+            </BrowserRouter>
+          </SidebarProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>
 )
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { accountSlice } from "@/features/accounts/slice/account.slice.ts"
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage"
+import accountReducer from "@/features/accounts/slice/account.slice.ts"
+import { combineReducers } from "redux"
 
-export default configureStore({
-  reducer: {
-    accountSlice: accountSlice.reducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
+
+export type Dispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof rootReducer>
+export const persistor = persistStore(store)
