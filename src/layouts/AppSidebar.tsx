818efe8ae459def01f70e14a38d90ed078e926ba
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-11 21:40:06 +0530

    Added some extra fields to the sidebar.

--- src/layouts/AppSidebar.tsx
+++ src/layouts/AppSidebar.tsx
@@ -6,15 +6,9 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuBadge,
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar.tsx"
-import { HugeiconsIcon } from "@hugeicons/react"
-import {
-  ArrowDataTransferVerticalIcon,
-  Home03Icon,
-} from "@hugeicons/core-free-icons"
 
 const AppSidebar: React.FC = () => {
   return (
@@ -23,20 +17,97 @@ const AppSidebar: React.FC = () => {
         Finance Master
       </SidebarHeader>
       <SidebarContent>
+        {/* First group starts */}
         <SidebarGroup>
-          <SidebarGroupLabel>Main</SidebarGroupLabel>
+          <SidebarGroupLabel>Finance</SidebarGroupLabel>
           <SidebarMenu>
             <SidebarMenuItem>
               <SidebarMenuButton>Overview</SidebarMenuButton>
-              <SidebarMenuBadge>
-                <HugeiconsIcon icon={Home03Icon} size={20} />
-              </SidebarMenuBadge>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
             </SidebarMenuItem>
             <SidebarMenuItem>
-              <SidebarMenuButton>Transactions</SidebarMenuButton>
-              <SidebarMenuBadge>
-                <HugeiconsIcon icon={ArrowDataTransferVerticalIcon} size={20} />
-              </SidebarMenuBadge>
+              <SidebarMenuButton>My Accounts</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Financial Overview</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Credit Cards</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Transfers</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={ArrowDataTransferVerticalIcon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        {/* First group ends */}
+
+        {/* Second group starts */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Planning</SidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Monthly Plan</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Subscriptions</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Recurring Expenses</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+        {/* Second group ends */}
+
+        {/* Third group ends */}
+        <SidebarGroup>
+          <SidebarGroupLabel>Loans and Assets</SidebarGroupLabel>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Loans</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Lendings</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>My Assets</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
+            </SidebarMenuItem>
+            <SidebarMenuItem>
+              <SidebarMenuButton>Saving Goals</SidebarMenuButton>
+              {/*<SidebarMenuBadge>*/}
+              {/*  <HugeiconsIcon icon={Home03Icon} size={20} />*/}
+              {/*</SidebarMenuBadge>*/}
             </SidebarMenuItem>
           </SidebarMenu>
         </SidebarGroup>
