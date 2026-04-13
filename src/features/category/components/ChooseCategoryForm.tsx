1285e1189c18527c272a41e819094e36c35a5578
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-13 14:03:19 +0530

    Added input fields to create new category in ChooseCategoryForm.tsx

--- src/features/category/components/ChooseCategoryForm.tsx
+++ src/features/category/components/ChooseCategoryForm.tsx
@@ -2,6 +2,11 @@ import React from "react"
 import { Button } from "@/components/ui/button.tsx"
 import { categories } from "@/features/category/constants/category.constants.ts"
 import useCategoryStore from "@/features/category/store/category.store.ts"
+import { AddSquareIcon } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
+import { Field, FieldGroup, FieldSet } from "@/components/ui/field.tsx"
+import { Label } from "@/components/ui/label.tsx"
+import { Input } from "@/components/ui/input.tsx"
 
 const ChooseCategoryForm: React.FC = () => {
   const categoryStore = useCategoryStore()
@@ -11,14 +16,28 @@ const ChooseCategoryForm: React.FC = () => {
         "fixed top-0 left-0 flex h-full w-full items-center justify-center backdrop-blur-2xl"
       }
     >
-      <div className={"bg-secondary p-5"}>
+      <div className={"relative bg-secondary p-5"}>
         <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
           Select Category
         </h4>
         <p className="mb-5 text-muted-foreground">
           Choose a category for this transaction
         </p>
-        <div className={"grid w-full grid-cols-5 grid-rows-5 gap-1"}>
+        <FieldGroup className={"mb-2"}>
+          <FieldSet>
+            <Field orientation={"horizontal"}>
+              <Field>
+                <Label>Create new category</Label>
+                <Input />
+              </Field>
+              <Field>
+                <Label>Comment</Label>
+                <Input />
+              </Field>
+            </Field>
+          </FieldSet>
+        </FieldGroup>
+        <div className={"grid w-full grid-cols-5 gap-1"}>
           {categories.map((category) => (
             <Button
               onClick={() => categoryStore.setSelectedCategory(category.name)}
@@ -29,6 +48,13 @@ const ChooseCategoryForm: React.FC = () => {
             </Button>
           ))}
         </div>
+        <Button
+          size={"icon-lg"}
+          variant={"ghost"}
+          className={"absolute top-0 right-0 p-2"}
+        >
+          <HugeiconsIcon icon={AddSquareIcon} />
+        </Button>
       </div>
     </div>
   )
