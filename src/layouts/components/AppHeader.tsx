1d98d2745ca590f3245109f771a55c7b0609cb48
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-15 21:12:43 +0530

    Added a section in the app header to show overall balance.

--- src/layouts/components/AppHeader.tsx
+++ src/layouts/components/AppHeader.tsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { Search } from "lucide-react"
 
 const AppHeader: React.FC = () => {
   return (
@@ -7,16 +6,12 @@ const AppHeader: React.FC = () => {
       id="header"
       className="flex h-20 w-full items-center justify-between border-b border-gray-200 bg-white px-5"
     >
-      <div className="bg-container flex items-center justify-center rounded-md px-3 py-2">
-        {/* icon */}
-        <Search color="#8B8B8B" size={20} />
-        {/* input field */}
-        <input
-          type="text"
-          className="ml-3 w-[200px] border-none outline-none"
-          placeholder="Search"
-        />
-      </div>
+      <p className={"text-muted"}>
+        Overall Balance{" "}
+        <span className={"text-l font-semibold !text-destructive"}>
+          Rs 2,000
+        </span>
+      </p>
     </header>
   )
 }
