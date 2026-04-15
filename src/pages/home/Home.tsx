498bc4e442da99ea8a7401bfb755078d8f2eec30
Kaveesha Sashen <sanaakaveesha@gmail.com>
2026-04-15 18:35:35 +0530

    Created a list view to display recent transactions on the landing page.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import AccountCard from "@/features/accounts/components/AccountCard.tsx"
+import { House } from "lucide-react"
 
 const Home: React.FC = () => {
-  const arr = [1, 1, 3, 3, 3, 3, 3]
+  const arr = [1, 1, 3, 3, 1, 3, 1]
   return (
     <div className="flex h-screen w-full overflow-hidden font-poppins text-secondary">
       <div className="h-full w-[250px] overflow-y-auto border-r border-gray-200 p-3">
@@ -15,7 +16,7 @@ const Home: React.FC = () => {
           Finance Master
         </h1>
       </div>
-      <div className="h-full flex-1 overflow-y-auto bg-primary p-3">
+      <div className="h-full flex-1 overflow-y-auto bg-primary p-5">
         {/* Accounts */}
         <div className={"h-full w-full"}>
           <div
@@ -37,6 +38,33 @@ const Home: React.FC = () => {
               <AccountCard key={i} name={"HNB"} amount={1250} />
             ))}
           </div>
+          <div className={"mt-5 mb-10 w-full"}>
+            <h1 className={"mb-3 text-lg font-medium"}>Recent Transactions</h1>
+            <div
+              className={
+                "mb-10 h-[600px] w-[500px] overflow-y-auto rounded-lg border border-gray-200 bg-white p-3"
+              }
+            >
+              {arr.map((_, i) => (
+                <div
+                  className={`flex w-full items-center justify-between gap-3 ${i === arr.length - 1 ? "border-none" : "border-b"} border-gray-200 p-3`}
+                >
+                  <div className={"flex items-center gap-3"}>
+                    <p className={"rounded-lg bg-accent p-2"}>
+                      <House color={"white"} />
+                    </p>
+                    <div>
+                      <p className={"font-medium"}>Category</p>
+                      <p className={"text-sm text-muted"}>Account</p>
+                      <p className={"text-sm text-muted italic"}>Comment</p>
+                    </div>
+                  </div>
+                  <p className={"text-lg font-medium"}>Rs 12</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className={"h-10"}></div>
         </div>
         {/* Recent Transactions */}
       </div>
